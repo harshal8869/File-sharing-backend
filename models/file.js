b5d554a4233c28a79391ec4659ba0d61536f8928
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-08-11 01:05:17 +0530

    Update file.js

--- models/file.js
+++ models/file.js
@@ -7,7 +7,7 @@ const fileSchema = new mongoose.Schema({
     },
     name: { 
         type: String,
-        required: true
+        required: true  
     },
     downloadContent: {
         type: Number,
